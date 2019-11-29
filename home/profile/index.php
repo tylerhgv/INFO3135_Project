c1070d82eb47336f3c5f7493a50ccf47432f2ac7
Tung Hoang <tunghv4@fsoft.com.vn>
2019-11-29 11:47:51 -0800

    Add header handlers to home/profile/index.php

--- home/profile/index.php
+++ home/profile/index.php
@@ -30,10 +30,34 @@ require('../../model/dog_db.php');
         $_SESSION['profile_dogs'] = $user_dogs;
 
         //for each dog a person has, add the session variables
-        
-        
 
-        //Load the profile page
-        header('location: profile_view.php');
+    }
+
+    // script to handle header action
+    if(isset($_GET['action']) && $_GET['action'] != ""){
+        $action = $_GET['action'];
+        if(isset($action) && $action != ""){
+            switch($action){
+                case "home":
+                    header('location: ../index.php');
+                    break;
+                case "group":
+                    header('location: ../group/index.php');
+                    break;
+                case "event":
+                    header('location: ../event/index.php');
+                    break;
+                case "signout":
+                    header('location: ../../auth/signout.php');
+                    break;
+                case "profile":
+                default:
+                    header('location: profile_view.php');
+                    exit();
+            }
+        }
+    } else {
+      header('location: profile_view.php');
+      exit();
     }
 ?>
