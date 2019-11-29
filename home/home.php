b8e3e2b5f5ecb687476872e1f39e560ad5f8ff71
Tung Hoang <tunghv4@fsoft.com.vn>
2019-11-28 21:05:17 -0800

    Make home/home.php the view and home/index.php the controller

---
+++ home/home.php
@@ -0,0 +1,6 @@
+<?php include '../view/header.php'; ?>
+<?php
+session_start();
+echo session_id();
+?>
+<?php include '../view/footer.php'; ?>
--- home/index.php
+++ home/index.php
@@ -1,7 +1,37 @@
-<?php 
+<?php
 session_start();
-echo session_id();
 
-echo '<a href="profile/index.php">Profile</a>'
+//Check if the user is signed in
+if(empty($_SESSION['signedin']) || $_SESSION['signedin'] != true){
+    header('location: ../index.php');
+    exit();
+}
 
+// script to handle header action
+if(isset($_GET['action']) && $_GET['action'] != ""){
+    $action = $_GET['action'];
+    if(isset($action) && $action != ""){
+        switch($action){
+            case "profile":
+                header('location: profile.index.php');
+                break;
+            case "group":
+                header('location: group/index.php');
+                break;
+            case "event":
+                header('location: event/index.php');
+                break;
+            case "signout":
+                header('location: ../auth/signout.php');
+                break;
+            case "home":
+            default:
+                header('location: home.php');
+                exit();
+        }
+    }
+} else {
+  header('location: home.php');
+  exit();
+}
 ?>
